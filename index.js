5fbe8e82223063be3d166f47bdd96f47042af41c
IvanML19 <ivanmartinlopez19@hotmail.com>
2017-05-18 22:27:17 +0200

    index

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
-const Product = require('./models/product')
+const ProductCtrl = require('./controllers/product')
 
 const app = express()
 const port = 3000
@@ -12,46 +12,12 @@ const port = 3000
 app.use(bodyParser.urlencoded( {extended: false} ))
 app.use(bodyParser.json())
 
-app.get('/api/product', (req, res) => {
+app.get('/api/product', ProductCtrl.getProducts(req, res))
 
-	Product.find({}, (err, products) => {
-		if (err) return res.status(500).send( { message: `Error al recuperar el producto: ${err}` } )
-		if (!products) return res.status(404).send( { message: `No existen productos` } )
-
-		// res.status(200).send( { product: product })
-		res.status(200).send( { products })
-	})
-})
-
-app.get('/api/product/:productId', (req, res) => {
-	let productId = req.params.productId
-
-	Product.findById(productId, (err, product) => {
-		if (err) return res.status(500).send( { message: `Error al recuperar el producto: ${err}` } )
-		if (!product) return res.status(404).send( { message: `El producto no existe` } )
-
-		// res.status(200).send( { product: product })
-		res.status(200).send( { product })
-	})
-})
+app.get('/api/product/:productId', (req, res))
 
 app.post('/api/product', (req, res) => {
-	console.log('POST /api/product')
-	console.log(req.body)
-
-	let product = new Product()
-	product.name = req.body.name
-	product.picture = req.body.picture
-	product.price = req.body.price
-	product.category = req.body.category
-	product.description = req.body.description
-
-	product.save((err, productStored) => {
-		if (err) {
-			res.status(500).send( { message: `Error al guardar en la BD: ${err}` })
-		}
-		res.status(200).send({ product: productStored })
-	})
+	
 })
 
 app.put('/api/product/:productId', (req, res) => {
